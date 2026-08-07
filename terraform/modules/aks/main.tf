397323d0875ec5cddb2f4a5229d7a6604a4af4ba
venkata bolisetti <venkata.pda@gmail.com>
2026-08-06 23:31:16 -0400

    added tfvards6

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count = var.node_count
     vm_size    = var.vm_size
     #auto_scaling_enabled = true
-    min_count = var.node_min_count
+    min_count = 0
     max_count = var.node_max_count
     vnet_subnet_id = var.aks_subnet_id
     os_sku = "Ubuntu"
