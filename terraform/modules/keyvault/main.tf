1313ce2f708106b0193c9b052799b34905e1cd76
venkata bolisetti <venkata.pda@gmail.com>
2026-08-06 22:57:30 -0400

    added tfvards3

--- terraform/modules/keyvault/main.tf
+++ terraform/modules/keyvault/main.tf
@@ -1,29 +1,44 @@
+data "azurerm_client_config" "current" {}
+
 data "azurerm_key_vault" "kv" {
   name                = var.key_vault_name
   resource_group_name = var.key_vault_resource_group_name
+}
+
+resource "azurerm_role_assignment" "key_vault_secrets_officer" {
+  scope                = data.azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_key_vault_secret" "postgres_host" {
   name         = "postgres-host"
   value        = var.postgres_host
   key_vault_id = data.azurerm_key_vault.kv.id
-  
+
+  depends_on = [azurerm_role_assignment.key_vault_secrets_officer]
 }
 
 resource "azurerm_key_vault_secret" "postgres_username" {
   name         = "postgres-username"
   value        = var.postgres_admin_username
   key_vault_id = data.azurerm_key_vault.kv.id
+
+  depends_on = [azurerm_role_assignment.key_vault_secrets_officer]
 }
 
 resource "azurerm_key_vault_secret" "postgres_password" {
   name         = "postgres-password"
   value        = var.postgres_admin_password
   key_vault_id = data.azurerm_key_vault.kv.id
+
+  depends_on = [azurerm_role_assignment.key_vault_secrets_officer]
 }
 
 resource "azurerm_key_vault_secret" "postgres_database" {
   name         = "postgres-database-name"
   value        = var.postgres_database_name
   key_vault_id = data.azurerm_key_vault.kv.id
+
+  depends_on = [azurerm_role_assignment.key_vault_secrets_officer]
 }
